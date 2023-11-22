3b0a6e411538970df9c05a4087683c1cc29792c9
odafc <151117046+odafc@users.noreply.github.com>
2023-11-22 14:18:40 +0900

    Update app.js

--- test/app.js
+++ test/app.js
@@ -10,6 +10,78 @@ const createPlaces = ({ latitude, longitude }) => [
     },
   }));
   
+  const models = [
+    {
+      url: '#asset-eevee',
+      scale: ['2', '2', '2'],
+      info: 'Eevee',
+    }
+  ];
+  
+  const createEntity = ({ id, location: { latitude, longitude }, model, scale: [x, y, z] }) => {
+    const $entity = document.createRange().createContextualFragment(`
+      <a-entity
+        id="${id}"
+        gltf-model="${model}"
+        scale="${x} ${y} ${z}"
+        gps-entity-place="latitude: ${latitude}; longitude: ${longitude};"
+        animation="property: rotation; to: 0 360 0; dur: 4000; easing: linear; loop: true"
+      ></a-entity>
+    `);
+  
+    $entity.addEventListener(
+      'loaded',
+      () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')),
+    );
+  
+    return $entity;
+  };
+  
+  const renderPlace = ({ location }, count) => {
+    const { url, scale, info } = models[count % models.length];
+  
+    const $info = document.querySelector('.info');
+    $info.innerText = info;
+  
+    const $scene = document.querySelector('a-scene');
+    const entityId = `e-${Math.floor(Math.random() * 100000)}`;
+    const $entity = createEntity({ id: entityId, location, model: url, scale });
+    $scene.appendChild($entity);
+  
+    // const $button = document.querySelector('button[data-action="change"]');
+    // const buttonClickEventListener = () => {
+    //   $button.removeEventListener('click', buttonClickEventListener, false);
+    //   $scene.removeChild(document.getElementById(entityId));
+    //   renderPlace({ location }, count + 1);
+    // }
+    // $button.addEventListener('click', buttonClickEventListener);
+  };
+  
+  const main = async () => {
+    console.log('main');
+  
+    const $info = document.querySelector('.info');
+    $info.innerText = 'loading...'
+  
+    const successCallback = position => {
+      console.log('success', position);
+      createPlaces(position.coords)
+        .forEach(place => renderPlace(place, 0));
+    };
+  
+    const errorCallback = error => {
+      console.log('error', error);
+      alert(error.message);
+    };
+  
+    navigator.geolocation.getCurrentPosition(
+      successCallback,
+      errorCallback
+    );
+  };
+  
+  window.onload = main;
+  
   const createEntity = ({ location: { latitude, longitude }, model, scale: [x, y, z] }) => {
     const $entity = document.createRange().createContextualFragment(`
       <a-entity
