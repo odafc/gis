9037aca3ba420dd76497f38cf8141f148d61d9fa
odafc <151117046+odafc@users.noreply.github.com>
2023-11-24 14:44:10 +0900

    Update app.js

--- test/app.js
+++ test/app.js
@@ -1,67 +1,67 @@
 const staticLoadPlaces = () => [
-    // {
-    //   label: 'p1',
-    //   location: {
-    //     latitude:   35.1910560565511,
-    //     longitude: 132.500876966642,
-    //   },
-    // },
-    // {
-    //     label: 'p2',
-    //     location: {
-    //       latitude:   35.191084200685, 
-    //       longitude: 132.500892342173,
-    //     },
-    //   },
-    //     {
-    //     label: 'p3',
-    //     location: {
-    //       latitude:   35.1911149669384, 
-    //       longitude: 132.500903277536,
-    //     },
-    //   },
-    //      {
-    //     label: 'p4',
-    //     location: {
-    //       latitude:   35.1911447120686, 
-    //       longitude:  132.500905942735,
-    //     },
-    //   },
-    //      {
-    //     label: 'p5',
-    //     location: {
-    //       latitude:   35.1911750002241, 
-    //       longitude: 132.500900980012,
-    //     },
-    //   },
-    //   {
-    //     label: 'p6',
-    //     location: {
-    //       latitude:   35.1912026908251, 
-    //       longitude: 132.500891555726,
-    //     },
-    //   },
-    //   {
-    //     label: 'p7',
-    //     location: {
-    //       latitude:   35.1912304077455, 
-    //       longitude: 132.50087259385,
-    //     },
-    //   },
-    //   {
-    //     label: 'p8',
-    //     location: {
-    //       latitude:   35.1912654532629, 
-    //       longitude: 132.500846031813,
-    //     },
-    //   },
-    //  {
-    //     label: 'p9',
-    //     location: {
-    //       latitude:   35.1913059240663, 
-    //       longitude: 132.50083017269,
-    //     },
-    //   },
+    {
+      label: 'p1',
+      location: {
+        latitude:   35.1910560565511,
+        longitude: 132.500876966642,
+      },
+    },
+    {
+        label: 'p2',
+        location: {
+          latitude:   35.191084200685, 
+          longitude: 132.500892342173,
+        },
+      },
+        {
+        label: 'p3',
+        location: {
+          latitude:   35.1911149669384, 
+          longitude: 132.500903277536,
+        },
+      },
+         {
+        label: 'p4',
+        location: {
+          latitude:   35.1911447120686, 
+          longitude:  132.500905942735,
+        },
+      },
+         {
+        label: 'p5',
+        location: {
+          latitude:   35.1911750002241, 
+          longitude: 132.500900980012,
+        },
+      },
+      {
+        label: 'p6',
+        location: {
+          latitude:   35.1912026908251, 
+          longitude: 132.500891555726,
+        },
+      },
+      {
+        label: 'p7',
+        location: {
+          latitude:   35.1912304077455, 
+          longitude: 132.50087259385,
+        },
+      },
+      {
+        label: 'p8',
+        location: {
+          latitude:   35.1912654532629, 
+          longitude: 132.500846031813,
+        },
+      },
+     {
+        label: 'p9',
+        location: {
+          latitude:   35.1913059240663, 
+          longitude: 132.50083017269,
+        },
+      },
 
   ];
   
