254afc9b2088ee6d350ba3ea61f54ab79084088b
odafc <151117046+odafc@users.noreply.github.com>
2024-04-03 15:46:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -39,30 +39,8 @@ var map = L.map('mapid').setView([35.161, 132.435], 17);
         var gj = L.geoJson(kawakita, {
             style: function (feature) {
                 return { fillColor: "#00f", color: "#f00", weight: 3, opacity: 0.6, fillOpacity: 0.1, };
-            },
-            // onEachFeature: function (j, layer) {
-            //     let p = j.properties;
-            //     if (p) {
-            //         let name = p.name, desc = p.description;
-            //         let popup = "<h3>" + name + "</h3>" + "<p>" + desc + "</p>";
-            //         layer.bindPopup(popup);
-            //     }
-            // }
+            }
         });
-
-        // var oda = L.geoJson(nameUoda, {
-        //     style: function (feature) {
-        //         return { fillColor: "#fff", color: "#f00", weight: 3, opacity: 0.6, fillOpacity: 0, };
-        //     },
-        //     onEachFeature: function (j, layer) {
-        //         let p = j.properties;
-        //         if (p) {
-        //             let name = p.name, desc = p.description;
-        //             let popup = "<h3>" + name + "</h3>";
-        //             layer.bindPopup(popup);
-        //         }
-        //     }
-        // });
 
 
         var baseCtl = { // ベースマップ切替ボタンの定義
@@ -78,7 +56,6 @@ var map = L.map('mapid').setView([35.161, 132.435], 17);
             "1970年頃写真": overMap[5],
             "1960年頃写真": overMap[6],
             "川北線": gj,
-            // "字等別境界": oda,
         };
 
 
