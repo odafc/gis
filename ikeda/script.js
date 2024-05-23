34351232d14451ca5217b88980ea97b29ca3912d
odafc <151117046+odafc@users.noreply.github.com>
2024-05-23 12:08:12 +0900

    Update script.js

--- ikeda/script.js
+++ ikeda/script.js
@@ -73,65 +73,6 @@ var map = L.map('mapid').setView([ 35.12735 , 132.56318], 15);
         L.control.opacityLayers(baseCtl, overCtl, { collapsed: true }).addTo(map); // 透過付マップ切替
 
 
-var options = {
-            latitudeText: "緯度",
-            longitudeText: "経度",
-            precision: 5,
-            position: 'bottomright'
-        }
-        var cCtrl = new L.Control.Coordinates(options);
-        cCtrl.addTo(map);
-        map.on('click', function (e) {
-            cCtrl.setCoordinates(e);
-        });
-
-        (function () {
-            function b() {
-                var a = window,
-                    c = e;
-                if (a.addEventListener) a.addEventListener("load", c, !1);
-                else if (a.attachEvent) a.attachEvent("onload", c);
-                else {
-                    var d = a.onload;
-                    a.onload = function () {
-                        c.call(this);
-                        d && d.call(this)
-                    }
-                }
-            };
-            var f = !1;
-
-            function e() {
-                if (!f) {
-                    f = !0;
-                    for (var a = document.getElementsByClassName("psa_add_styles"), c = 0, d; d =
-                        a[c]; ++c)
-                        if ("NOSCRIPT" == d.nodeName) {
-                            var k = document.createElement("div");
-                            k.innerHTML = d.textContent;
-                            document.body.appendChild(k)
-                        }
-                }
-            }
-
-            function g() {
-                var a = window.requestAnimationFrame || window.webkitRequestAnimationFrame ||
-                    window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
-                    null;
-                a ? a(function () {
-                    window.setTimeout(e, 0)
-                }) : b()
-            }
-            var h = ["pagespeed", "CriticalCssLoader", "Run"],
-                l = this;
-            h[0] in l || !l.execScript || l.execScript("var " + h[0]);
-            for (var m; h.length && (m = h.shift());) h.length || void 0 === g ? l[m] ? l =
-                l[m] : l = l[m] = {} : l[m] = g;
-        })();
-        pagespeed.CriticalCssLoader.Run();
-
-
-
         // add location control to global name space for testing only
         // on a production site, omit the "lc = "!
         lc = L.control
