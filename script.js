1348d5ea4398b182e59f8be3752df6573b2a5a23
odafc <151117046+odafc@users.noreply.github.com>
2024-05-27 08:20:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,5 @@
 var map = L.map('mapid').setView([35.192, 132.499], 15);
 
-        var baseMap = [ // ベースマップの定義
-            L.tileLayer("https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png", {
-                minZoom: 5, maxZoom: 18
-            }),
-                
-　          L.tileLayer("https://cm02.mapion.co.jp/m2/tile/{z}/{x}/{y}.png?usr=atlas_org&amp;v=2.6", {
-                minZoom: 6, maxZoom: 19
-            }),              
-
-        ];
-
-        var overMap = [ // オーバーレイマップの定義
-            L.tileLayer("https://ariill-design.jp/xyz/kihon/{z}/{x}/{y}.png", {
-                minZoom: 16, maxZoom: 19
-            }),
-            L.tileLayer("https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}", {
-                minZoom: 2, maxZoom: 19
-            }),
-            L.tileLayer("https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.jpg", {
-                minZoom: 2, maxZoom: 18
-            }),
-            L.tileLayer("https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg", {
-                minZoom: 2, maxZoom: 18
-            }),
-            L.tileLayer("https://cyberjapandata.gsi.go.jp/xyz/gazo1/{z}/{x}/{y}.jpg", {
-                minZoom: 10, maxZoom: 17
-            }),
-            L.tileLayer("https://cyberjapandata.gsi.go.jp/xyz/ort_old10/{z}/{x}/{y}.png", {
-                minZoom: 2, maxZoom: 17
-            }),
-            L.tileLayer("https://mars.navitime.co.jp/mars/tile/v1/satellite/256/{z}/{y}/{x}", {
-                minZoom: 2, maxZoom: 19
-            }),
-        ];
-
-        // var oda = L.geoJson(nameUoda, {
-        //     style: function (feature) {
-        //         return { fillColor: "#fff", color: "#f00", weight: 3, opacity: 0.6, fillOpacity: 0, };
-        //     },
-        //     onEachFeature: function (j, layer) {
-        //         let p = j.properties;
-        //         if (p) {
-        //             let name = p.name, desc = p.description;
-        //             let popup = "<h3>" + name + "</h3>";
-        //             layer.bindPopup(popup);
-        //         }
-        //     }
-        // });
-
-
         var baseCtl = { // ベースマップ切替ボタンの定義
             "地理院標準地図": baseMap[0],
            　"マピオン": baseMap[1],
@@ -127,11 +77,3 @@ var map = L.map('mapid').setView([35.192, 132.499], 15);
                 l[m] : l = l[m] = {} : l[m] = g;
         })();
         pagespeed.CriticalCssLoader.Run();
-
-
-        // add location control to global name space for testing only
-        // on a production site, omit the "lc = "!
-        lc = L.control
-            .locate({
-            })
-            .addTo(map);
