e1ab3c1774c744539905f32f953ba8d59b0e2504
odafc <151117046+odafc@users.noreply.github.com>
2024-05-27 08:44:42 +0900

    Update common.js

--- common.js
+++ common.js
@@ -5,12 +5,12 @@
             L.tileLayer("https://cm02.mapion.co.jp/m2/tile/{z}/{x}/{y}.png?usr=atlas_org&amp;v=2.6", {
                 minZoom: 6, maxZoom: 19
             }),
+         　　L.tileLayer("https://ariill-design.jp/xyz/kihon/{z}/{x}/{y}.png", {
+                minZoom: 16, maxZoom: 19
+            }),
         ];
 
         var overMap = [ // オーバーレイマップの定義
-            L.tileLayer("https://ariill-design.jp/xyz/kihon/{z}/{x}/{y}.png", {
-                minZoom: 16, maxZoom: 19
-            }),
             L.tileLayer("https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}", {
                 minZoom: 2, maxZoom: 19
             }),
