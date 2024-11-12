7e675daf7c06b562b711ef44f1c46da59194c3dc
odafc <151117046+odafc@users.noreply.github.com>
2024-11-12 13:17:36 +0900

    Update script.js

--- kankan/script.js
+++ kankan/script.js
@@ -22,7 +22,6 @@ var baseCtl = { // ベースマップ切替ボタンの定義
 var overCtl = { // オーバーレイマップ切替ボタンの定義
     "Google最新写真": overMap[0],
     "Esri航空写真": overMap[1],
-    "NTT航空写真": overMap[5],
     "地理院最新写真": overMap[2],
     "1970年頃写真": overMap[3],
     "1960年頃写真": overMap[4],
